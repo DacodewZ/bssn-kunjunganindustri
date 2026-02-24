a40a7ba2280560276e22e4fb9577a51cb5ef3fc3
DacodewZ <mdafaashidiqashidiq@gmail.com>
2026-02-24 13:16:17 +0700

    0.2

--- index.js
+++ index.js
@@ -19,3 +19,55 @@ function showSlides() {
     dots[slideIndex - 1].classList.add("active");
     setTimeout(showSlides, 5000); 
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const countElement = document.getElementById('count');
+    const infoRow = document.querySelector('.data-info-row');
+
+    const targetNumber = 133000000;
+    const duration = 2000; 
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                let startTimestamp = null;
+                const step = (timestamp) => {
+                    if (!startTimestamp) startTimestamp = timestamp;
+                    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+                    const currentCount = Math.floor(progress * targetNumber);
+                    countElement.innerText = currentCount.toLocaleString('id-ID');
+                    
+                    if (progress < 1) {
+                        window.requestAnimationFrame(step);
+                    } else {
+                        countElement.innerText = targetNumber.toLocaleString('id-ID') + "+";
+                        if(infoRow) {
+                            infoRow.classList.add('reveal');
+                        }
+                    }
+                };       
+                window.requestAnimationFrame(step);
+                observer.unobserve(entry.target); 
+            }
+        });
+    }, { threshold: 0.5 }); 
+    if (countElement) {
+        observer.observe(countElement);
+    }
+});
+
+document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+
+        const currentItem = button.parentElement;
+        const isOpen = currentItem.classList.contains('active');
+
+        document.querySelectorAll('.faq-item').forEach(item => {
+            item.classList.remove('active');
+        });
+
+        if (!isOpen) {
+            currentItem.classList.add('active');
+        }
+    });
+});
